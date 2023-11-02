0584c79b3fddf6d4be53c410085d87c669bbf492
Kyle Stultz <kylostultz@gmail.com>
2023-11-02 16:06:02 -0500

    Added images. Edited index.html for images to appear. Updated scripts.js and styles.css

--- scripts.js
+++ scripts.js
@@ -1,1 +1,17 @@
 /* Add your JavaScript to this file */
+function ValidateEmail(email)
+{
+var mailformat = /^w+([.-]?w+)*@w+([.-]?w+)*(.w{2,3})+$/;
+if(inputText.value.match(mailformat))
+{
+alert("Thank You! Your email address", email, "has been added to our mailing list!");
+document.form1.text1.focus();
+return true;
+}
+else
+{
+alert("Please enter a valid email address.");
+document.form1.text1.focus();
+return false;
+}
+}
